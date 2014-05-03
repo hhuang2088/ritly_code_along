e7e97ca67ac16923fc92d9e7671ea59fd43e711e
Henry Huang <hhuang2088@gmail.com>
2014-05-02 22:29:48 -0700

    shows method and view created

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -2,4 +2,16 @@ class UrlsController < ApplicationController
   def new
     @url = Url.new
   end
+
+  def create 
+    url = Url.create(link: params[:url][:link])
+    url.random_string = SecureRandom.urlsafe_base64(6)
+    url.save
+    redirect_to "/urls/#{url.id}"
+  end
+
+  def show
+    @url = Url.find(params[:id])
+  end
+
 end
