eebf600b7accd6d1c3406f0f0de57790a51f1098
Henry Huang <hhuang2088@gmail.com>
2014-05-02 22:37:32 -0700

    Adding presence and uniqueness validations to the url model

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -4,14 +4,23 @@ class UrlsController < ApplicationController
   end
 
   def create 
-    url = Url.create(link: params[:url][:link])
+    url = Url.create url_params
     url.random_string = SecureRandom.urlsafe_base64(6)
-    url.save
-    redirect_to "/urls/#{url.id}"
+    if url.save
+      redirect_to url
+    else
+      flash[:error] = "The url could not be saved. Try again"
+      redirect_to root_path
+    end
   end
 
   def show
     @url = Url.find(params[:id])
   end
+
+  private 
+    def url_params
+      params.require(:url).permit(:link)
+    end
 
 end
